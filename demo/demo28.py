9a7f540f8e2e808e6210acf9985fdebd1590503f
chenpeiyuan1987 <chenpeiyuan1987@aliyun.com>
2017-06-09 09:21:17 +0800

    add demo/demo28.py

---
+++ demo/demo28.py
@@ -0,0 +1,12 @@
+# 字符串判断
+
+str = input("请输入内容：");
+print("isalnum",str.isalnum());
+print("isalpha",str.isalpha());
+print("isdigit",str.isdigit());
+print("islower",str.islower());
+print("isupper",str.isupper());
+print("istitle",str.istitle());
+print("isspace",str.isspace());
+print("isnumeric",str.isnumeric());
+print("isdecimal",str.isdecimal());
