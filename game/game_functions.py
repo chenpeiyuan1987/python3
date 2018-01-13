26c6acc83bdd8593f8541674eb9751890b538469
chenpeiyuan <chenpeiyuan1987@163.com>
2018-01-13 14:50:51 +0800

    fire bullet

--- game/game_functions.py
+++ game/game_functions.py
@@ -1,8 +1,9 @@
 import sys
 
 import pygame
+from bullet import Bullet
 
-def check_events(ship):
+def check_events(ai_settings, screen, ship, bullets):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit();
@@ -12,6 +13,10 @@ def check_events(ship):
                 ship.moving_right = True;
             elif event.key == pygame.K_LEFT:
                 ship.moving_left = True;
+            elif event.key == pygame.K_SPACE:
+                if len(bullets) < ai_settings.bullets_allowed:
+                    new_bullet = Bullet(ai_settings, screen, ship);
+                    bullets.add(new_bullet);
 
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
@@ -20,8 +25,18 @@ def check_events(ship):
                 ship.moving_left = False;
 
 
-def update_screen(ai_settings, screen, ship):
+def update_screen(ai_settings, screen, ship, bullets):
     screen.fill(ai_settings.bg_color);
+    for bullet in bullets.sprites():
+        bullet.draw_bullet()
     ship.blitme();
 
     pygame.display.flip();
+
+def update_bullets(bullets):
+    bullets.update();
+
+    for bullet in bullets.copy():
+        if bullet.rect.bottom <= 0:
+            bullets.remove(bullet)
+#    print(len(bullets))
