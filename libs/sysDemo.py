f394bfc3590cf0af2cc72659e1fd246455ddb085
chenpeiyuan <chenpeiyuan1987@163.com>
2018-01-13 14:09:03 +0800

    add sysDemo.py

---
+++ libs/sysDemo.py
@@ -0,0 +1,37 @@
+import sys
+
+print(str(sys.argv))
+
+'''
+len(o)
+str[n]
+str[-n]
+str[n:m]
+str[n:]
+str[:m]
+
+_
+
+str(o)
+
+list = []
+
+if:
+elif:
+else:
+
+for n in range(1,10):
+
+pass
+
+in
+
+推导式
+x*2 for x in range(10) if x % 2 == 0
+
+del
+list = [1,2,3];
+'''
+
+
+
