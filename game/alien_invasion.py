a636105e409288dd1363180754250f7ce3790bcc
chenpeiyuan <chenpeiyuan1987@163.com>
2018-01-25 09:11:15 +0800

    add alien

--- game/alien_invasion.py
+++ game/alien_invasion.py
@@ -3,6 +3,7 @@ import pygame;
 
 from settings import Settings;
 from ship import Ship;
+from alien import Alien;
 import game_functions as gf
 from pygame.sprite import Group
 
@@ -15,12 +16,15 @@ def run_game():
 
     ship = Ship(ai_settings, screen);
     bullets = Group()
+
+    aliens = Group()
+    gf.create_fleet(ai_settings, screen, ship, aliens)
+    
     #
     while True:
         gf.check_events(ai_settings, screen, ship, bullets);
         ship.update();
         gf.update_bullets(bullets)
-        
-        gf.update_screen(ai_settings, screen, ship, bullets);
+        gf.update_screen(ai_settings, screen, ship, aliens, bullets);
 
 run_game();
