a3d9199824eca3b6e9f5c5f77c56523f703cbd0c
chenpeiyuan <chenpeiyuan1987@163.com>
2018-04-15 11:18:46 +0800

    edit django

--- django/learning_logs/urls.py
+++ django/learning_logs/urls.py
@@ -5,5 +5,8 @@ app_name = 'learning_logs'
 urlpatterns = [
     path('', views.index, name='index'),
     path('topics/', views.topics, name='topics'),
-    path('topics/<int:id>/', views.topic, name='topic'),
+    path('topic/<int:id>/', views.topic, name='topic'),
+    path('new_topic/', views.new_topic, name='new_topic'),
+    path('new_entry/<topic_id>', views.new_entry, name='new_entry'),
+    path('edit_entry/<id>', views.edit_entry, name='edit_entry'),
 ]
--- django/ll_env/Scripts/django-admin.py
+++
@@ -1,5 +0,0 @@
-#!e:\python3\django\ll_env\scripts\python.exe
-from django.core import management
-
-if __name__ == "__main__":
-    management.execute_from_command_line()
